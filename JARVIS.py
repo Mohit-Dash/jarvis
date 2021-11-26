216e30da98adbedac8f304c54037e8e811128eff
Mohit-Dash <94890831+Mohit-Dash@users.noreply.github.com>
2021-11-26 09:46:30 +0530

    Update JARVIS.py

--- JARVIS.py
+++ JARVIS.py
@@ -1,29 +1,98 @@
-import pyttsx3
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
 import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
 
-engine=pyttsx3.init('sapi5')
-voices=engine.getProperty('voices')
-# print(voices[0].id)
-engine.setProperty('voice',voices[0].id)
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
 
 def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
 
-    engine.say(audio) 
-
-    engine.runAndWait() 
 
 def wishMe():
-
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
         speak("Good Morning!")
 
     elif hour>=12 and hour<18:
-        speak("Good Afternoon!")
+        speak("Good Afternoon!")   
 
     else:
-        speak("Good Evening!")    
+        speak("Good Evening!")  
 
-    speak("I am Jarvis.How may I help you?")
-if __name__=="__main__" :
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('mddash0912@gmail.com', 'smarty@0912')
+    server.sendmail('pradyumnreddy@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
     wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'play music in spotify' in query:
+            SongPath = r"C:\Users\MOHIT DASH\OneDrive\Desktop\Spotify.lnk"
+            os.startfile(SongPath)
+                
+            
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = r"C:\Users\MOHIT DASH\OneDrive\Desktop\Visual Studio Code.lnk"
+            os.startfile(codePath)
